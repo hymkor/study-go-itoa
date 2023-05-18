46e4741ccd997a156a74126d212d2bb15d79c76f
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-05-18 21:38:01 +0900

    Add test

--- main_test.go
+++ main_test.go
@@ -3,7 +3,26 @@ package itoa
 import (
 	"io"
 	"testing"
+	"strings"
 )
+
+func TestUp1(t *testing.T) {
+	var buffer strings.Builder
+	up1(100, &buffer)
+	result := buffer.String()
+	if result != "\x1B[100A" {
+		t.Fatalf("result=%v", result)
+	}
+}
+
+func TestUp2(t *testing.T) {
+	var buffer strings.Builder
+	up2(100, &buffer)
+	result := buffer.String()
+	if result != "\x1B[100A" {
+		t.Fatalf("result=%v", result)
+	}
+}
 
 func BenchmarkUp1(b *testing.B) {
 	for i := 0; i < b.N; i++ {
